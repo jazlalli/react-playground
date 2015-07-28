a698c970d2154240ad64d84b6db7dace88f6945e
Jaz Lalli <jaz@pebblecode.com>
2015-07-28 14:10:21 +0100

    fake data

--- controllers/whisky.js
+++ controllers/whisky.js
@@ -1,8 +1,8 @@
 var db = require('../data/db');
 
-// function randomFlavour() {
-//   return Math.floor(Math.random() * 4);
-// }
+function randomFlavour() {
+  return Math.floor(Math.random() * 4);
+}
 
 exports.getAll = function getAll(callback) {
   db.getAllWhiskies(function (err, result) {
@@ -10,26 +10,26 @@ exports.getAll = function getAll(callback) {
       return callback(err);
     }
 
-    // result
-    //   .sort(function (a, b) {
-    //     if (a.name < b.name) return -1;
-    //     else if (b.name < a.name) return 1;
-    //     else return 0;
-    //   })
-    //   .forEach(function (item) {
-    //     item.sweetness = randomFlavour();
-    //     item.fruity = randomFlavour();
-    //     item.floral = randomFlavour();
-    //     item.body = randomFlavour();
-    //     item.smokey = randomFlavour();
-    //     item.tobacco = randomFlavour();
-    //     item.medicinal = randomFlavour();
-    //     item.winey = randomFlavour();
-    //     item.spicey = randomFlavour();
-    //     item.malty = randomFlavour();
-    //     item.nutty = randomFlavour();
-    //     item.honey = randomFlavour();
-    //   });
+    result
+      .sort(function (a, b) {
+        if (a.name < b.name) return -1;
+        else if (b.name < a.name) return 1;
+        else return 0;
+      })
+      .forEach(function (item) {
+        item.sweetness = randomFlavour();
+        item.fruity = randomFlavour();
+        item.floral = randomFlavour();
+        item.body = randomFlavour();
+        item.smokey = randomFlavour();
+        item.tobacco = randomFlavour();
+        item.medicinal = randomFlavour();
+        item.winey = randomFlavour();
+        item.spicey = randomFlavour();
+        item.malty = randomFlavour();
+        item.nutty = randomFlavour();
+        item.honey = randomFlavour();
+      });
 
     return callback(null, {data: result});
   });
